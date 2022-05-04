2812c4fd15bdfa49484c2524cc4b572d7108a170
Matheus Alexandrino <57869203+Matheus-Alexandrino@users.noreply.github.com>
2022-05-03 22:31:20 -0300

    Commit

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,8 +1,9 @@
 import { CloseButton } from "./CloseButton";
-
+import { useState } from "react";
 import bugImageUrl from '../assets/bug.svg';
 import ideaImageUrl from '../assets/idea.svg';
 import thoughtImageUrl from '../assets/thought.svg';
+
 
 
 const feedbackTypes = {
@@ -29,7 +30,6 @@ const feedbackTypes = {
     },
 };
 
-
 // Object.entries(feedbackTypes)=>
 /**
  *  [
@@ -39,11 +39,16 @@ const feedbackTypes = {
  * ]
  */
 
+type FeedbackType = keyof typeof feedbackTypes;
+
 export function WidgetForm() {
+    const [feedbackType, setFeedbackType] = useState<FeedbackType | (null)
+
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             <header>
-                <span className="text-xl leading-6"> Deixe deu Feedback</span>
+                <span className="text-xl leading-6"> Deixe seu Feedback</span>
 
                 <CloseButton />
             </header>
@@ -55,7 +60,7 @@ export function WidgetForm() {
                             key={key}
                             className=" bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex-col items-center gap-2 border-2 border-transparent
                              hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                            //onClick={}
+                            onClick={() => setFeedbackType(key} //função
                             type="button"
                         >
                             <img src={value.image.source} alt={value.image.alt}  />
