f3984e53000771c3f0c9233c7f5066bca9476008
Matheus Alexandrino <57869203+Matheus-Alexandrino@users.noreply.github.com>
2022-05-03 22:47:02 -0300

    commit

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,8 +1,8 @@
-import { CloseButton } from "./CloseButton";
+import { CloseButton } from "../CloseButton";
 import { useState } from "react";
-import bugImageUrl from '../assets/bug.svg';
-import ideaImageUrl from '../assets/idea.svg';
-import thoughtImageUrl from '../assets/thought.svg';
+import bugImageUrl from '../../assets/bug.svg';
+import ideaImageUrl from '../../assets/idea.svg';
+import thoughtImageUrl from '../../assets/thought.svg';
 
 
 
@@ -53,14 +53,15 @@ export function WidgetForm() {
                 <CloseButton />
             </header>
 
-            <div className="flex py-8 gap-2 w-full"> 
+           {!feedbackType ?(
+                <div className="flex py-8 gap-2 w-full"> 
                 { Object.entries(feedbackTypes).map(([key,value]) => {
                     return (
                         <button
                             key={key}
                             className=" bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex-col items-center gap-2 border-2 border-transparent
                              hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                            onClick={() => setFeedbackType(key} //função
+                            onClick={() => setFeedbackType(key as FeedbackType)} //função
                             type="button"
                         >
                             <img src={value.image.source} alt={value.image.alt}  />
@@ -69,6 +70,9 @@ export function WidgetForm() {
                     )
                 }) }
             </div>
+           ) : (
+               <p>Hello World!</p>
+           )}
 
             <footer className="text-xs text-neutral-400">
                 Feito por <a className="underline underline-offset-2" href="https://github.com/Matheus-Alexandrino">Matheus💻Alexandrino</a>
