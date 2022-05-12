08e95c3949c635e415c2158df96494594966f3f0
Matheus Alexandrino <57869203+Matheus-Alexandrino@users.noreply.github.com>
2022-05-12 01:13:25 -0300

    commit

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,5 @@
 import { ArrowLeft, Camera, Rocket, TwitterLogo } from "phosphor-react";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes} from "..";
 import { CloseButton } from "../../CloseButton"
 import { ScreenshotButton } from "../ScreenshotButton";
@@ -9,13 +9,23 @@ interface FeedbackContentStepProps {
     onFeedbackRestartRequested: () => void;
 }
 export function FeedbackContentStep({
-     feedbackType,
+    feedbackType,
     onFeedbackRestartRequested
     }: FeedbackContentStepProps) {
 
 const [screenshot, setScreenshot] = useState<string | null>(null)
-
+const [comment, setComment] = useState('');
     const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+
+    function handleSubmitFeedback(event: FormEvent) {
+        event.preventDefault();
+        console.log({
+            screenshot,
+            comment
+        })
+    }
+
     return (
         <> 
          <header>
@@ -32,24 +42,28 @@ const [screenshot, setScreenshot] = useState<string | null>(null)
 
                 <CloseButton />
          </header>
-         <form className="my-4 w-full">
+
+         <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
              <textarea
                 className="min-w-[304px] w-full min-h[112px] text-sm placeholder-zinc-400
                  text-zinc-100 border-zinc-600 bg-transparent rounded-md
                   focus:border-brand-500 focus:ring-brand-500 focus:ring-1 resize-none
                   focus:outline-none scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
                 placeholder="Conte com detalhes o que está acontecendo..."
+                onChange={event => setComment(event.target.value)} //traz o texto do textarea
              />
 
              <footer className="flex gap-2 mt-2">
                 <ScreenshotButton 
+                    screenshot={screenshot}
                     onScreenshotTook={setScreenshot}
                 /> 
                  <button
                     type="submit"
+                    disabled={comment.length == 0}
                     className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center 
                     items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2
-                    focus:ring-offset-zinc-900 focus:ring-brand-500 transiction-colors"
+                    focus:ring-offset-zinc-900 focus:ring-brand-500 transiction-colors disabled:opacity-50 disabled:hover:bg-brand-500"
                  >
                      Enviar Feedback 
                  </button>
