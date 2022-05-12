b4c6d009cddd327b2dbfd6978500481df719b5a2
Matheus Alexandrino <matwsalmeida@gmail.com>
2022-05-12 20:41:18 -0300

    commit ApiREST

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,8 +2,12 @@ import express from 'express'
 
 const app = express();
 
-app.get('/users', (req, res) => { //controle de rotas
-    return res.send('Phalla Devs!')
+
+// GET
+
+
+app.post('/feedbacks', (req, res) => { //controle de rotas
+    return res.send('Phalla Dev!')
 })
 app.listen(3333, () => {
     console.log('HTTP server running!')
