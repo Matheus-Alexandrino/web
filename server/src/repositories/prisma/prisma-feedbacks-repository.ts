af2c9dfb2ef40fbbbb466df222f262993f405d72
Matheus Alexandrino <matwsalmeida@gmail.com>
2022-05-12 23:08:01 -0300

    commit

---
+++ server/src/repositories/prisma/prisma-feedbacks-repository.ts
@@ -0,0 +1,7 @@
+import { FeedbackRepository } from "../feedbacks-repository";
+
+export class PrismaFeedbackRepository implements FeedbackRepository {
+    create: (data: FeedbackCreateData) {
+        
+    }
+}
